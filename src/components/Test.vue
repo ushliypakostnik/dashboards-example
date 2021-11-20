4fd544ddf35b4da86fb91f3899db249b304a1f0a
ushliypakostnik <levon.gambaryan@gmail.com>
2021-11-20 18:14:52 +0300

    test library with persist store

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -10,6 +10,16 @@
     <br />
     <br />
 
+    {{ test }}
+
+    <br />
+    <br />
+
+    {{ layout }}
+
+    <br />
+    <br />
+
     <div>
       <Link path="https://www.npmjs.com/package/ui-library-starter-test"
         >Library package on NPM</Link
@@ -25,47 +35,73 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
 import resize from 'ui-library-starter-test/src/mixins/resize.js';
 
 export default {
   name: 'Test',
 
   mixins: [resize],
+
+  computed: {
+    ...mapGetters(['test', 'layout']),
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.setTest('Изменяем стор библиотеки из проекта!');
+      this.setLayout('Изменяем стор библиотеки из проекта!');
+    }, 2000);
+  },
+
+  methods: {
+    ...mapActions(['setTest', 'setLayout']),
+  },
 };
 </script>
 
 <style lang="stylus" scoped>
-@import "~/src/stylus/_stylebase.styl";
+@import '~/src/stylus/_stylebase.styl';
 
-.test
-  padding 100px 0
-  text-align center
+.test {
+  padding: 100px 0;
+  text-align: center;
 
-  +$mobile()
-   padding 60px 0
+  +$mobile() {
+    padding: 60px 0;
+  }
 
-  &__logo
-    max-width 120px
-    margin 0 auto
+  &__logo {
+    max-width: 120px;
+    margin: 0 auto;
 
-    +$mobile()
-      max-width 80px
+    +$mobile() {
+      max-width: 80px;
+    }
+  }
 
-  &__components
-    margin-top 50px
+  &__components {
+    margin-top: 50px;
+  }
 
-  &__wrapper-inner
-    +$mobile()
-      padding 0 20px
+  &__wrapper-inner {
+    +$mobile() {
+      padding: 0 20px;
+    }
+  }
+}
 
-h1
-  $text("elena")
+h1 {
+  $text('elena');
 
-  +$tablet()
-    padding 0 40px
-    $text("olga")
+  +$tablet() {
+    padding: 0 40px;
+    $text('olga');
+  }
 
-  +$mobile()
-    padding 0 20px
-    $text("anna")
+  +$mobile() {
+    padding: 0 20px;
+    $text('anna');
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import ComponentLibrary from 'ui-library-starter-test';
 
 import 'ui-library-starter-test/dist/ui-library-starter-test.css';
 
+import store from 'ui-library-starter-test/src/store/index.js';
+
 Vue.use(ComponentLibrary);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   render: (h) => h(App),
 }).$mount('#app');
